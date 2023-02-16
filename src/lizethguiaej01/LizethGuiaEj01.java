530fd2fb6189242ee38c92059e82eec512edb042
Lizzanny <lizethvmartinezg@gmail.com>
2023-02-15 20:30:58 -0600

    Carpeta de encuentros

---
+++ src/lizethguiaej01/LizethGuiaEj01.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lizethguiaej01;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author LizzW
+ */
+public class LizethGuiaEj01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+	
+    }
+}
